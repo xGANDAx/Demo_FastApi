9ca21788fb181cf93bf0dc176d0bd505f1132e41
Jonathan <jonathanchitivam@gmail.com>
2024-11-20 15:16:12 -0500

    mend
    Add testing workflow

--- src/web/web.py
+++ src/web/web.py
@@ -1,9 +1,16 @@
+"""
+Module
+"""
+
 from fastapi import APIRouter
 
-router = APIRouter() 
+router = APIRouter()
+
 
 @router.get("")
 @router.get("/")
 async def hello_world():
+    """
+    Funtion that returns a "Hello, World!" message.
+    """
     return "Hello, World!"
-
